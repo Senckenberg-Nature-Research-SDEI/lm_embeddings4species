09206c8ae73af1da9ceaf947e09cdd9181db8314
sefika <sefika@sefikas-MacBook-Pro.local>
2026-06-08 15:47:16 +0200

    add new models and results

--- run_metrics.sh
+++ run_metrics.sh
@@ -30,9 +30,9 @@
 #   --input_folder results/S-BioBERT/ \
 #   --output_file results/S-BioBERT/metrics_summary.csv
 
-python src/evaluation/metric.py \
-  --input_folder results/PatentSBERTa/ \
-  --output_file results/PatentSBERTa/metrics_summary.csv
+# python src/evaluation/metric.py \
+#   --input_folder results/PatentSBERTa/ \
+#   --output_file results/PatentSBERTa/metrics_summary.csv
 
 # python src/evaluation/metric.py \
 #   --input_folder results/scibert_scivocab_cased/ \
@@ -46,3 +46,47 @@ python src/evaluation/metric.py \
 # python src/evaluation/metric.py \
 #   --input_folder results/sciNCL/ \
 #   --output_file results/sciNCL/metrics_summary.csv
+
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-large-zh-v1.5/ \
+#   --output_file results/bge-large-zh-v1.5/metrics_summary.csv
+
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-large-en/ \
+#   --output_file results/bge-large-en/metrics_summary.csv
+
+
+# python src/evaluation/metric.py \
+#   --input_folder results/llm-embedder/ \
+#   --output_file results/llm-embedder/metrics_summary.csv
+
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-reranker-v2-m3/ \
+#   --output_file results/bge-reranker-v2-m3/metrics_summary.csv
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-reranker-v2-gemma/ \
+#   --output_file results/bge-reranker-v2-gemma/metrics_summary.csv
+
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-reranker-large/ \
+#   --output_file results/bge-reranker-large/metrics_summary.csv
+
+# python src/evaluation/metric.py \
+#   --input_folder results/bge-reranker-base/ \
+#   --output_file results/bge-reranker-base/metrics_summary.csv
+
+
+
+python src/evaluation/metric.py \
+  --input_folder results/ModernBERT-large/ \
+  --output_file results/ModernBERT-large/metrics_summary.csv
+
+
+python src/evaluation/metric.py \
+  --input_folder results/ModernBERT-bio-large/ \
+  --output_file results/ModernBERT-bio-large/metrics_summary.csv
--- run_model_balanced.sh
+++ run_model_balanced.sh
@@ -1,138 +1,258 @@
-# SciBERT (uncased)
+# # SciBERT (uncased)
+# python src/models/mlp_balanced.py \
+#   --model-name "allenai/scibert_scivocab_uncased" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/scibert_scivocab_uncased/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "allenai/scibert_scivocab_uncased" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/scibert_scivocab_uncased/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # SciBERT (cased)
+# python src/models/mlp_balanced.py \
+#   --model-name "allenai/scibert_scivocab_cased" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/scibert_scivocab_cased/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "allenai/scibert_scivocab_cased" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/scibert_scivocab_cased/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # BiomedBERT
+# python src/models/mlp_balanced.py \
+#   --model-name "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/BiomedBERT/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/BiomedBERT/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # S-BioBERT
+# python src/models/mlp_balanced.py \
+#   --model-name "pritamdeka/S-BioBERT-snli-multinli-stsb" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/S-BioBERT/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "pritamdeka/S-BioBERT-snli-multinli-stsb" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/S-BioBERT/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # all-MiniLM-L6-v2
+# python src/models/mlp_balanced.py \
+#   --model-name "sentence-transformers/all-MiniLM-L6-v2" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/all-MiniLM-L6-v2/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "sentence-transformers/all-MiniLM-L6-v2" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/all-MiniLM-L6-v2/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # all-mpnet-base-v2
+# python src/models/mlp_balanced.py \
+#   --model-name "sentence-transformers/all-mpnet-base-v2" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/all-mpnet-base-v2/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "sentence-transformers/all-mpnet-base-v2" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/all-mpnet-base-v2/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # BGE Base
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-base-en-v1.5" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-base-en-v1.5/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-base-en-v1.5" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-base-en-v1.5/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # E5 Base v2
+# python src/models/mlp_balanced.py \
+#   --model-name "intfloat/e5-base-v2" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/e5-base-v2/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "intfloat/e5-base-v2" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/e5-base-v2/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # SciNCL
+# python src/models/mlp_balanced.py \
+#   --model-name "malteos/scincl" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/scincl/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "malteos/scincl" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/scincl/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# # PatentSBERTa
+# python src/models/mlp_balanced.py \
+#   --model-name "AI-Growth-Lab/PatentSBERTa" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/PatentSBERTa/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "AI-Growth-Lab/PatentSBERTa" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/PatentSBERTa/synonym_results_coleop.csv" \
+#   --threshold 0.70
+#   # PatentSBERTa
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-large-zh-v1.5" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-large-zh-v1.5/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-large-zh-v1.5" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-large-zh-v1.5/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-large-en" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-large-en/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-large-en" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-large-en/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/llm-embedder" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/llm-embedder/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/llm-embedder" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/llm-embedder/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-v2-m3" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-reranker-v2-m3/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-v2-m3" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-reranker-v2-m3/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-v2-gemma" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-reranker-v2-gemma/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-v2-gemma" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-reranker-v2-gemma/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-large" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-reranker-large/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-large" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-reranker-large/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-base" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/bge-reranker-base/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "BAAI/bge-reranker-base" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/bge-reranker-base/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
+# python src/models/mlp_balanced.py \
+#   --model-name "almanach/ModernBERT-bio-large" \
+#   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
+#   --output "results/ModernBERT-bio-large/synonym_results_lepo.csv" \
+#   --threshold 0.70
+
+# python src/models/mlp_balanced.py \
+#   --model-name "almanach/ModernBERT-bio-large" \
+#   --file "data/balanced_synonym_coleop_dataset.csv" \
+#   --output "results/ModernBERT-bio-large/synonym_results_coleop.csv" \
+#   --threshold 0.70
+
+
 python src/models/mlp_balanced.py \
-  --model-name "allenai/scibert_scivocab_uncased" \
+  --model-name "answerdotai/ModernBERT-large" \
   --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/scibert_scivocab_uncased/synonym_results_lepo.csv" \
+  --output "results/ModernBERT-large/synonym_results_lepo.csv" \
   --threshold 0.70
 
 python src/models/mlp_balanced.py \
-  --model-name "allenai/scibert_scivocab_uncased" \
+  --model-name "answerdotai/ModernBERT-large" \
   --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/scibert_scivocab_uncased/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# SciBERT (cased)
-python src/models/mlp_balanced.py \
-  --model-name "allenai/scibert_scivocab_cased" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/scibert_scivocab_cased/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "allenai/scibert_scivocab_cased" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/scibert_scivocab_cased/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# BiomedBERT
-python src/models/mlp_balanced.py \
-  --model-name "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/BiomedBERT/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/BiomedBERT/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# S-BioBERT
-python src/models/mlp_balanced.py \
-  --model-name "pritamdeka/S-BioBERT-snli-multinli-stsb" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/S-BioBERT/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "pritamdeka/S-BioBERT-snli-multinli-stsb" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/S-BioBERT/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# all-MiniLM-L6-v2
-python src/models/mlp_balanced.py \
-  --model-name "sentence-transformers/all-MiniLM-L6-v2" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/all-MiniLM-L6-v2/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "sentence-transformers/all-MiniLM-L6-v2" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/all-MiniLM-L6-v2/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# all-mpnet-base-v2
-python src/models/mlp_balanced.py \
-  --model-name "sentence-transformers/all-mpnet-base-v2" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/all-mpnet-base-v2/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "sentence-transformers/all-mpnet-base-v2" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/all-mpnet-base-v2/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# BGE Base
-python src/models/mlp_balanced.py \
-  --model-name "BAAI/bge-base-en-v1.5" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/bge-base-en-v1.5/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "BAAI/bge-base-en-v1.5" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/bge-base-en-v1.5/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# E5 Base v2
-python src/models/mlp_balanced.py \
-  --model-name "intfloat/e5-base-v2" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/e5-base-v2/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "intfloat/e5-base-v2" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/e5-base-v2/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# SciNCL
-python src/models/mlp_balanced.py \
-  --model-name "malteos/scincl" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/scincl/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "malteos/scincl" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/scincl/synonym_results_coleop.csv" \
-  --threshold 0.70
-
-
-# PatentSBERTa
-python src/models/mlp_balanced.py \
-  --model-name "AI-Growth-Lab/PatentSBERTa" \
-  --file "data/balanced_synonym_lepidoptera_dataset.csv" \
-  --output "results/PatentSBERTa/synonym_results_lepo.csv" \
-  --threshold 0.70
-
-python src/models/mlp_balanced.py \
-  --model-name "AI-Growth-Lab/PatentSBERTa" \
-  --file "data/balanced_synonym_coleop_dataset.csv" \
-  --output "results/PatentSBERTa/synonym_results_coleop.csv" \
+  --output "results/ModernBERT-large/synonym_results_coleop.csv" \
   --threshold 0.70
