e0c2226fe6ed7bf54d9db83e4eebfd0ea49b328b
Sefika Efeoglu <sefeoglu@MB-K1GV17JYMC.local>
2026-07-10 19:34:50 +0200

    add results

--- run_plant_make_balanced.sh
+++ run_plant_make_balanced.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
 # Rosales
-python src/data_preparation/make_balanced_dataset.py \
-  --input_csv data/rosales_taxa_with_current_names_all.csv \
-  --output_csv data/balanced_synonym_rosales_dataset.csv \
-  --legacy_col oldSpeciesCanonicalName \
-  --accepted_col species \
-  --taxon_id_col key
-
-# Fabales
-python src/data_preparation/make_balanced_dataset.py \
-  --input_csv data/fabales_taxa_with_current_names_all.csv \
-  --output_csv data/balanced_synonym_fabales_dataset.csv \
-  --legacy_col oldSpeciesCanonicalName \
-  --accepted_col species \
-  --taxon_id_col key
-
-# Lamiales
-python src/data_preparation/make_balanced_dataset.py \
-  --input_csv data/lamiales_taxa_with_current_names_all.csv \
-  --output_csv data/balanced_synonym_lamiales_dataset.csv \
-  --legacy_col oldSpeciesCanonicalName \
-  --accepted_col species \
-  --taxon_id_col key
-
-# # Poales
 # python src/data_preparation/make_balanced_dataset.py \
-#   --input_csv data/poales_taxa_with_current_names_all.csv \
-#   --output_csv data/balanced_synonym_poales_dataset.csv \
+#   --input_csv data/rosales_taxa_with_current_names_all.csv \
+#   --output_csv data/balanced_synonym_rosales_dataset.csv \
 #   --legacy_col oldSpeciesCanonicalName \
 #   --accepted_col species \
 #   --taxon_id_col key
 
-# # Coleus
+# # Fabales
 # python src/data_preparation/make_balanced_dataset.py \
-#   --input_csv data/coleus_taxa_with_current_names_all.csv \
-#   --output_csv data/balanced_synonym_coleus_dataset.csv \
+#   --input_csv data/fabales_taxa_with_current_names_all.csv \
+#   --output_csv data/balanced_synonym_fabales_dataset.csv \
 #   --legacy_col oldSpeciesCanonicalName \
 #   --accepted_col species \
 #   --taxon_id_col key
+
+# # Lamiales
+# python src/data_preparation/make_balanced_dataset.py \
+#   --input_csv data/lamiales_taxa_with_current_names_all.csv \
+#   --output_csv data/balanced_synonym_lamiales_dataset.csv \
+#   --legacy_col oldSpeciesCanonicalName \
+#   --accepted_col species \
+#   --taxon_id_col key
+
+# Poales
+python src/data_preparation/make_balanced_dataset.py \
+  --input_csv data/poales_taxa_with_current_names_all.csv \
+  --output_csv data/balanced_synonym_poales_dataset.csv \
+  --legacy_col oldSpeciesCanonicalName \
+  --accepted_col species \
+  --taxon_id_col key
+
